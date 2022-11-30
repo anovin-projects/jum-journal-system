6639107c7b299e873ef0baa9a31e813431e081b6
Aleksej <anovski3@gmail.com>
2022-11-30 05:33:05 +0100

    Changed the design

--- journal/filters.py
+++ journal/filters.py
@@ -7,11 +7,11 @@ from django import forms
 class ManuscriptFilter(django_filters.FilterSet):
     citation_title = django_filters.CharFilter(lookup_expr='icontains', label='Title', widget=forms.TextInput(attrs={
         'placeholder': 'Title contains', 'class': 'form-control p-1 shadow-sm w-100 my-2 mx-2',
-        'style': 'border-radius:10px;'}))
+        'style': 'border-radius:6px; font-weight:300;'}))
 
     keywords = django_filters.CharFilter(lookup_expr='icontains', label='Keywords', widget=forms.TextInput(attrs={
         'placeholder': 'Keywords contain', 'class': 'shadow-sm form-control p-1 w-100 my-2 mx-2', 'type': 'text',
-        'style': 'border-radius:10px;'}))
+        'style': 'border-radius:6px; font-weight:300;'}))
 
     citation_volume = ModelChoiceFilter(
         queryset=Volume.objects.all(),
@@ -19,18 +19,18 @@ class ManuscriptFilter(django_filters.FilterSet):
         label='Volume',
         widget=forms.Select(attrs={
             'placeholder': 'Volume', 'class': 'shadow-sm form-control p-1 shadow-sm w-100 my-2 mx-2',
-            'style': 'border-radius:10px;'}))
+            'style': 'border-radius:6px; font-weight:300;'}))
 
     section = ModelChoiceFilter(
         queryset=Sections.objects.all(),
         empty_label='Select Section',
         label='Section',
         widget=forms.Select(attrs={
-            'class': 'shadow-sm form-control p-1 shadow-sm w-100 my-2 mx-2', 'style': 'border-radius:10px;'}))
+            'class': 'shadow-sm form-control p-1 shadow-sm w-100 my-2 mx-2', 'style': 'border-radius:6px; font-weight:300;'}))
 
     abstract = django_filters.CharFilter(lookup_expr='icontains', label='Title', widget=forms.TextInput(attrs={
         'placeholder': 'Abstract contains', 'class': 'form-control p-1 shadow-sm w-100 my-2 mx-2 ',
-        'style': 'border-radius:10px;'}))
+        'style': 'border-radius:6px; font-weight:300;'}))
 
     class Meta:
         model = Manuscript
